5ad9d1dccbf8aaff3ea3db49e529a5131f5b8f1d
Kairos-T <80029462+Kairos-T@users.noreply.github.com>
2023-07-07 18:23:21 +0800

    Added paths

--- bot.py
+++ bot.py
@@ -2,14 +2,17 @@ import os
 import random
 import discord
 from discord.ext import commands
-#import requests
-#import json
 
 from decouple import Config
+
 config = Config('megabot.env')
 api_token = config.get('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
 def get_image(image_type):
     if image_type == 'cute':
         image_dir = os.path.join('megabot', 'cute')
@@ -21,7 +24,13 @@ def get_image(image_type):
         image_files = os.listdir(image_dir)
         image_file = random.choice(image_files)
         return os.path.join(image_dir, image_file)
-
+    elif image_type == 'goofy':
+        image_dir = os.path.join('megabot', 'goofy')
+        image_files = os.listdir(image_dir)
+        image_file = random.choice(image_files)
+        return os.path.join(image_dir, image_file)
+    else:
+        return 'default_dog.jpg'
 
 @client.event
 async def on_ready():
@@ -30,20 +39,19 @@ async def on_ready():
 @client.command()
 async def cute(ctx):
     # Call the get_image() function with the 'cute' argument and send the image to the channel
-    image_path = os.path.join('megabot', 'cute', get_image('cute'))
+    image_path = get_image('cute')
     await ctx.send(file=discord.File(image_path))
 
 @client.command()
 async def sleep(ctx):
     # Call the get_image() function with the 'sleep' argument and send the image to the channel
-    image_path = os.path.join('megabot', 'sleep', get_image('sleep'))
+    image_path = get_image('sleep')
     await ctx.send(file=discord.File(image_path))
 
 @client.command()
 async def goofy(ctx):
     # Call the get_image() function with the 'goofy' argument and send the image to the channel
-    image_path = os.path.join('megabot', 'goofy', get_image('goofy'))
+    image_path = get_image('goofy')
     await ctx.send(file=discord.File(image_path))
-
 
 client.run(api_token)
