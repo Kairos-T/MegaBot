38326a5f4ddd34b744931817760d8d9dfae2fda7
Kairos-T <80029462+Kairos-T@users.noreply.github.com>
2023-07-08 14:42:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,12 @@ from decouple import Config
 config = Config('megabot.env')
 api_token = config.get('DISCORD_TOKEN')
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 intents.members = True
 #intents.members_content = True
 BOT_CHANNEL = 1097083507710369862
 
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 def get_image(image_type):
     if image_type == 'cute':
@@ -35,20 +35,20 @@ def get_image(image_type):
     else:
         return 'default_dog.jpg'
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
     try:
-        synced = await client.tree.sync()
+        synced = await bot.tree.sync()
         print(f"Synced {len(synced)} Command(s)")
     except Exception as e:
         print(e)
 
 # EVENTS
-@client.event
+@bot.event
 async def on_member_join(member):
-    channel = client.get_channel(BOT_CHANNEL)
+    channel = bot.get_channel(BOT_CHANNEL)
     await channel.send(f'Welcome {member.name}! Please introduce yourself!')
     """name = member.name
     pfp = member.display.avatar
@@ -59,41 +59,40 @@ async def on_member_join(member):
     embed.set.footer(text = "This is a footer :D")
     await channel.send(embed=embed)"""
 
-@client.event
+@bot.event
 async def on_member_remove(member):
-    channel = client.get.channel(BOT_CHANNEL)
+    channel = bot.get.channel(BOT_CHANNEL)
     await channel.send(f'{member.name} has left the server.')
 
-@client.event
+@bot.event
 async def on_message(message):
     author = message.author
     content = message.content
-    await client.process_commands(message)
+    await bot.process_commands(message)
     print(f'{author}: {content}')
-    #await client.get_channel(BOT_CHANNEL).send(f'{author}: {content}')
+    #await bot.get_channel(BOT_CHANNEL).send(f'{author}: {content}')
 
-@client.event
+@bot.event
 async def on_message_delete(message):
     author = message.author
     content = message.content
     channel = message.channel
-    await client.channel.send(f'{author} deleted "{content}" from {channel}')
+    await bot.channel.send(f'{author} deleted "{content}" from {channel}')
 
-@client.event
+@bot.event
 async def on_message_edit(before, after):
     author = before.author
     channel = before.channel
     before_content = before_content
     after_content = after_content
     await channel.send(f"Before: {before_content}\nAfter: {after_content}")
-    #await client.get_channel(BOT_CHANNEL).send(f'{author} edited "{before_content}" to "{after_content}" in {channel}')
 
-@client.event
+@bot.event
 async def on_message_edit(before, after):
-    if before.author == client.user:
+    if before.author == bot.user:
         return;
 
-@client.event
+@bot.event
 async def on_reaction_add(reaction, user):
     channel = reaction.message.channel
     name = user.name
@@ -101,7 +100,7 @@ async def on_reaction_add(reaction, user):
     content = reaction.message.content
     await channel.send(f'{name} reacted with {emoji} to "{content}"')
 
-@client.event
+@bot.event
 async def on_reaction_remove(reaction, user):
     channel = reaction.message.channel
     name = user.name
@@ -111,23 +110,27 @@ async def on_reaction_remove(reaction, user):
 
 
 # COMMANDS
+@bot.command()
+async def ping(ctx):
+    mention = await ctx.send('pong')
+    await mention.add_reaction('🏓')
 
-@client.command()
+@bot.command()
 async def cute(ctx):
     # Call the get_image() function with the 'cute' argument and send the image to the channel
     image_path = get_image('cute')
     await ctx.send(file=discord.File(image_path))
 
-@client.command()
+@bot.command()
 async def sleep(ctx):
     # Call the get_image() function with the 'sleep' argument and send the image to the channel
     image_path = get_image('sleep')
     await ctx.send(file=discord.File(image_path))
 
-@client.command()
+@bot.command()
 async def goofy(ctx):
     # Call the get_image() function with the 'goofy' argument and send the image to the channel
     image_path = get_image('goofy')
     await ctx.send(file=discord.File(image_path))
 
-client.run(api_token)
+bot.run(api_token)
